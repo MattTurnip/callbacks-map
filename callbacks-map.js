e2639b8fb6ad2ddcec004e8de6d6e14d9a7a609c
MattTurnip <mattadavey@gmail.com>
2019-03-07 20:41:36 +0000

    Higher Order Function works on array

--- callbacks-map.js
+++ callbacks-map.js
@@ -1,13 +1,33 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
-map(words, function(word) {
+
+var wordLength = map(words, function(word) {
   return word.length;
 });
 
-map(words, function(word) {
+var wordUpper = map(words, function(word) {
   return word.toUpperCase();
 });
 
-map(words, function(word) {
+var wordReverse = map(words, function(word) {
   return word.split('').reverse().join('');
 });
+
+
+//a higher order function (that means it takes functions as args.)
+function map(array, callback){
+    var results = []
+    for (let i = 0; i < array.length; i++) {
+        results.push(callback(array[i]))
+    }   
+    return results
+}
+
+console.log(wordLength);
+console.log(wordUpper);
+console.log(wordReverse);
+
+
+
+
+
